e868e92978af07b1212f4a54bed603e81c0a6d3d
Michael Barkdoll <mabarkdoll@siu.edu>
2018-07-04 00:48:19 -0500

    Add login method to AuthController.cs

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers {
     [Route ("api/[controller]")]
@@ -32,6 +38,38 @@ namespace DatingApp.API.Controllers {
             var createUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
             return StatusCode(201);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto) 
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            // In repository we return null if the password doesn't match
+            if (userFromRepo == null)
+                return Unauthorized();      // Don't tell them if the username exists or not.
+            
+            // generate JWT token 
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            // Need to create a key to sign our token, so it can be validated by the server
+            // For now as a hard coded string on our controller, but we'll soon remove it and put it in our configuration
+            // Key must be coded as a bytesarray
+            var key = Encoding.ASCII.GetBytes("super secret key");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                    new Claim(ClaimTypes.Name, userFromRepo.Username)
+                }),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), 
+                SecurityAlgorithms.HmacSha512Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok( new {tokenString});
         }
     }
 }
