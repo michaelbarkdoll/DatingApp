c7a1aec3ad63cbd6f323052e63ba289d554a6d14
Michael Barkdoll <mabarkdoll@siu.edu>
2018-07-04 22:45:11 -0500

    Added register() inside auth.service.ts.  Return 201 success to client.

--- DatingApp.SPA/src/app/_services/auth.service.ts
+++ DatingApp.SPA/src/app/_services/auth.service.ts
@@ -14,14 +14,26 @@ export class AuthService {
     // need a method to call our login method in our api
     // pass in username and password that we get from our login form
     login(model: any) {
-        const headers = new Headers({'Content-type': 'application/json'});
-        const options = new RequestOptions({headers: headers});
-        return this.http.post(this.baseUrl + 'login', model, options).map((response: Response) => {
+        // const headers = new Headers({'Content-type': 'application/json'});
+        // const options = new RequestOptions({headers: headers});
+        return this.http.post(this.baseUrl + 'login', model, this.requestOptions()).map((response: Response) => {
             const user = response.json();
             if (user) {
                 localStorage.setItem('token', user.tokenString);
                 this.userToken = user.tokenString;
             }
         });
+    }
+
+    register(model: any) {
+        // model just contains username and password at this point.
+        // All we're returning from this register method at the moment from the api is status code of 201
+        //  so we really don't need to pass anything back or map anything back to our component in this case.
+        return this.http.post(this.baseUrl + 'register', model, this.requestOptions());
+    }
+
+    private requestOptions() {
+        const headers = new Headers({'Content-type': 'application/json'});
+        return new RequestOptions({headers: headers});
     }
 }
--- DatingApp.SPA/src/app/register/register.component.ts
+++ DatingApp.SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,13 +11,18 @@ export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registration successful');
+    }, error => {
+      console.log(error);
+    });
+    //console.log(this.model);
   }
 
   cancel() {
