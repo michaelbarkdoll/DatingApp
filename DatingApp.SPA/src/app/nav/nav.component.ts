a324b38335fc92cb5b0d752d4c1e5bd40f073807
Michael Barkdoll <mabarkdoll@siu.edu>
2018-07-08 23:07:25 -0500

    Fix private access error!

--- DatingApp.SPA/src/app/nav/nav.component.ts
+++ DatingApp.SPA/src/app/nav/nav.component.ts
@@ -11,7 +11,8 @@ import { Router } from '@angular/router';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertifyService: AlertifyService, private router: Router) { }
+  // constructor(private authService: AuthService, private alertifyService: AlertifyService, private router: Router) { }
+  constructor(public authService: AuthService, private alertifyService: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
