ec0b29ed9a6232c238700bc7041ea0051545140a
Michael Barkdoll <mabarkdoll@siu.edu>
2018-07-21 22:01:29 -0500

    Add Advisor Form.

--- SIS.SPA/src/app/advisors/advisor-add/advisor-add.component.ts
+++ SIS.SPA/src/app/advisors/advisor-add/advisor-add.component.ts
@@ -3,6 +3,8 @@ import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { Router, ActivatedRoute } from '../../../../node_modules/@angular/router';
 import { Advisors } from '../../_models/Advisors';
+import { FormGroup, FormBuilder, Validators } from '../../../../node_modules/@angular/forms';
+import { AuthHttp } from '../../../../node_modules/angular2-jwt';
 
 @Component({
   selector: 'app-advisor-add',
@@ -11,14 +13,56 @@ import { Advisors } from '../../_models/Advisors';
 })
 export class AdvisorAddComponent implements OnInit {
   advisors: Advisors[];
+  addAdvisorForm: FormGroup;
 
-  constructor(private userService: UserService, private alertify: AlertifyService,
-    private route: ActivatedRoute, private router: Router) { }
+  constructor(private userService: UserService,
+    private alertify: AlertifyService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private authHttp: AuthHttp) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.advisors = data['advisors'];
     });
+
+    this.createaddAdvisorForm();
   }
+
+  createaddAdvisorForm() {
+    this.addAdvisorForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      fullName: ['', Validators.required],
+      title: ['', Validators.required]
+     });
+    // }, { validator: this.Validator });
+  }
+
+   Validator(formGroup: FormGroup) {
+    return formGroup.get('firstName').value === formGroup.get('firstName').value ? null : {'mismatch' : true};
+
+    // return formGroup.get('password').value === formGroup.get('confirmPassword').value ? null : {'mismatch' : true};
+   }
+
+  /*
+  {
+  "firstName": "Michael",
+  "lastName": "Barkdoll",
+  "fullName": "Michael Barkdoll",
+  "title": "Staff"
+  }
+  */
+
+ addAdvisor() {
+  if (this.addAdvisorForm.valid) {
+    console.log('Test');
+  }
+ }
+
+ cancel() {
+  this.router.navigate(['/advisors']);
+ }
 
 }
